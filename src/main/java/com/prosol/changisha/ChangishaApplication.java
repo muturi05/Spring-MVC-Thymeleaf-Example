759e591e45df69d77a6c054f9ea27cafeca0970b
muturi05 <jgatweku@gmail.com>
2021-04-30 11:33:10 +0300

    Spring MVC View with Thymeleaf, HTML, CSS

---
+++ src/main/java/com/prosol/changisha/ChangishaApplication.java
@@ -0,0 +1,13 @@
+package com.prosol.changisha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChangishaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChangishaApplication.class, args);
+    }
+
+}
